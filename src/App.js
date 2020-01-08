98cffa180d08cc7e29a5a0030519f84cf95f65c1
Jonathan Yuen <jonwyuen@gmail.com>
2020-01-07 23:44:55 -0800

    seedColors and initial Palette component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Palette from "Palette";
+import seedColors from "./seedColors";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <p>Color Picker</p>
+      <Palette palette={...seedColors[4]} />
     </div>
   );
 };
---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Palette = ({ palette }) => {
+  return (
+    <div className="Palette">
+      <div className="Palette-colors"></div>
+    </div>
+  );
+};
+
+export default Palette;
