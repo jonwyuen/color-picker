a1f58b6633648040e90c36deb65495388f3fa30b
Jonathan Yuen <jonwyuen@gmail.com>
2020-01-14 02:53:09 -0800

    styling PaletteList with makeStyles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,13 +1,51 @@
 import React from "react";
 import MiniPalette from "./MiniPalette";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center"
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    flexWrap: "wrap"
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white"
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%"
+  }
+});
 
 const PaletteList = ({ palettes }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>React Colors</h1>
-      {palettes.map(palette => (
-        <MiniPalette {...palette} />
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map(palette => (
+            <MiniPalette {...palette} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
