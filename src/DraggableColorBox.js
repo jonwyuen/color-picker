7a9f498bbdb6dd2b368863458596b82374b30b0d
Jonathan Yuen <jonwyuen@gmail.com>
2020-01-26 23:54:27 -0800

    styling DraggableColorBox

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,11 +1,15 @@
 import React from "react";
 import useStyles from "./styles/DraggableColorBoxStyles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const DraggableColorBox = ({ color, name }) => {
   const classes = useStyles();
   return (
     <div className={classes.root} style={{ backgroundColor: color }}>
-      {name}
+      <div className={classes.boxContent}>
+        <span>{name}</span>
+        <DeleteIcon className={classes.deleteIcon} />
+      </div>
     </div>
   );
 };
