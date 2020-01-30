fd323c8b7920941bba2d514478d9bf846879f016
Jonathan Yuen <jonwyuen@gmail.com>
2020-01-29 23:53:38 -0800

    adding a dialog to PaletteMetaForm

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import PaletteMetaForm from "./PaletteMetaForm";
 import clsx from "clsx";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -8,7 +9,6 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import useStyles from "./styles/PaletteFormNavStyles";
 
 const PaletteFormNav = ({
@@ -17,18 +17,8 @@ const PaletteFormNav = ({
   handleDrawerOpen,
   open
 }) => {
-  const [newPaletteName, setNewPaletteName] = useState("");
-
-  const handlePaletteNameChange = e => setNewPaletteName(e.target.value);
   const classes = useStyles({ drawerWidth: 400 });
 
-  useEffect(() => {
-    ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
-      palettes.every(
-        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
-      )
-    );
-  }, [palettes]);
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -54,22 +44,10 @@ const PaletteFormNav = ({
           </Typography>
         </Toolbar>
         <div className={classes.navBtns}>
-          <ValidatorForm onSubmit={() => handleSavePalette(newPaletteName)}>
-            <TextValidator
-              label="Palette Name"
-              name="newPaletteName"
-              value={newPaletteName}
-              onChange={handlePaletteNameChange}
-              validators={["required", "isPaletteNameUnique"]}
-              errorMessages={[
-                "Please enter a palette name",
-                "Palette name must be unique"
-              ]}
-            />
-            <Button variant="contained" color="primary" type="submit">
-              Save Palette
-            </Button>
-          </ValidatorForm>
+          <PaletteMetaForm
+            palettes={palettes}
+            handleSavePalette={handleSavePalette}
+          />
           <Link to="/">
             <Button variant="contained" color="secondary">
               Go Back
